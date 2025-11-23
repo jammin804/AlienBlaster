1c3c1c2dd350b3263474122d15ee7765956c28b0
Mike Harris <jammin804@gmail.com>
2025-11-23 18:27:07 -0500

    Jump Sprite Added
    Added Jump Sprite to the player

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,8 +7,18 @@ public class Player : MonoBehaviour
     [SerializeField] private float _horizontalVelocity = 3.0f;
     [SerializeField] private float _jumpVelocity = 5.0f;
     [SerializeField] private float _jumpDuration = 0.5f;
+    [SerializeField] private Sprite _jumpSprite;
+    private Sprite _defaultSprite;
     public bool IsGrounded;
+    private SpriteRenderer _spriteRenderer;
 
+
+    private void Awake()
+    {
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+        _defaultSprite = _spriteRenderer.sprite;
+        
+    }
 
     void OnDrawGizmos()
     {
@@ -21,16 +31,17 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        SpriteRenderer spriteRenderer = GetComponent<SpriteRenderer>();
-        Vector2 origin = new Vector2(transform.position.x, transform.position.y - spriteRenderer.sprite.bounds.extents.y);
+        Vector2 origin = new Vector2(transform.position.x, transform.position.y - _spriteRenderer.sprite.bounds.extents.y);
         var hit = Physics2D.Raycast(origin, Vector2.down, 0.1f);
         if (hit.collider != null)
         {
             IsGrounded = true;
+            _spriteRenderer.sprite = _defaultSprite;
         }
         else
         {
             IsGrounded = false;
+            _spriteRenderer.sprite = _jumpSprite;
         }
 
         var horizontal = Input.GetAxis("Horizontal");
