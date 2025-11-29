5b99fb34edc933b677163be3dcad2ce79858428e
Mike Harris <jammin804@gmail.com>
2025-11-29 15:10:26 -0500

    Main menu set up with load level buttons

---
+++ Assets/Scripts/LoadLevelButton.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadLevelButton : MonoBehaviour
+{
+    [SerializeField] private string _levelName;
+
+    public void Load()
+    {
+        SceneManager.LoadScene(_levelName);
+    }
+}
