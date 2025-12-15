ede7b4db9a94ff76d4413de7fdd7511235951600
Mike Harris <jammin804@gmail.com>
2025-12-15 12:00:30 -0500

    Added support for a single saved games

--- Assets/Scripts/NewGameButton.cs
+++ Assets/Scripts/NewGameButton.cs
@@ -3,14 +3,7 @@ using UnityEngine.UI;
 
 public class NewGameButton : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        GetComponent<Button>().onClick.AddListener(CreateNewGame);
-    }
+    void Start() => GetComponent<Button>().onClick.AddListener(CreateNewGame);
 
-    public void CreateNewGame()
-    {
-        GameManager.Instance.NewGame();
-    }
+    public void CreateNewGame() => GameManager.Instance.NewGame();
 }
